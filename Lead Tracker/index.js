e584f7fd96c28225e21a5e97eb4e5fa46be50951
ginni <jainginni23@gmail.com>
2023-06-15 23:18:03 +0530

    chrome extension added

---
+++ Lead Tracker/index.js
@@ -0,0 +1,40 @@
+// let myLeads = []
+// const inputEl = document.getElementById("input-el")
+// const inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click", function() {
+//     // Push the value from the inputEl into the myLeads array 
+//     // instead of the hard-coded "www.awesomeleads.com" value
+//     // Google -> "get value from input field javascript"
+//     myLeads.push(inputEl.value)
+//     console.log(myLeads)
+// })
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    // Clear out the input field
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+
+         // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+
+        
+    }
+    ulEl.innerHTML = listItems  
+}
+
+
+
+
